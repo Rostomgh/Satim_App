eb06c98794f4d8cc710c4966fd056e172a0e675a
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-09-23 12:05:35 +0100

    fix error endpoint,and cubit NavBar

--- lib/features/Auth/presentation/view/LoginScreen.dart
+++ lib/features/Auth/presentation/view/LoginScreen.dart
@@ -42,130 +42,128 @@ class _LoginScreenState extends State<LoginScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                verticalSpace(137),
-                Image.asset(Assets.LoginP),
-                verticalSpace(30),
-                const Center(
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      fontFamily: 'PoppinsRegular',
-                      color: AppColor.black,
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
+          child: BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is AuthLoading) {
+                _showLoadingDialog(context);
+                print('AuthLoading state detected');
+              } else if (state is AuthSuccess) {
+                _hideLoadingDialog(context);
+                Future.delayed(const Duration(milliseconds: 300), () {
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil('/nav', (route) => false);
+                  print('Navigating to /home');
+                });
+              } else if (state is AuthError) {
+                _hideLoadingDialog(context);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text(state.error)),
+                );
+                print('AuthError state detected: ${state.error}');
+              }
+            },
+            child: Form(
+              key: _formKey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    verticalSpace(137),
+                    Image.asset(Assets.LoginP),
+                    verticalSpace(30),
+                    const Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontFamily: 'PoppinsRegular',
+                          color: AppColor.black,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                verticalSpace(20),
-                const Padding(
-                  padding: EdgeInsets.only(left: 30.0, right: 30),
-                  child: Text(
-                    'Drop your username and password here',
-                    style: TextStyle(
-                      fontFamily: 'PoppinsRegular',
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
+                    verticalSpace(20),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 30.0, right: 30),
+                      child: Text(
+                        'Drop your username and password here',
+                        style: TextStyle(
+                          fontFamily: 'PoppinsRegular',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                verticalSpace(20),
-                CustomInput(
-                  hint: 'Email',
-                  obc: false,
-                  icon: Icons.email,
-                  keyboardType: TextInputType.emailAddress,
-                  mycontroller: emailController,
-                  valid: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Email cannot be empty';
-                    }
-                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                      return 'Enter a valid email';
-                    }
-                    return null;
-                  },
-                ),
-                verticalSpace(16),
-                CustomInput(
-                  hint: 'Password',
-                  obc: true,
-                  icon: Icons.lock,
-                  keyboardType: TextInputType.visiblePassword,
-                  mycontroller: passwordController,
-                  valid: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Password cannot be empty';
-                    }
-                    if (value.length < 6) {
-                      return 'Password must be at least 6 characters';
-                    }
-                    return null;
-                  },
-                ),
-                verticalSpace(11),
-                const Text(
-                  'Forget your password ?',
-                  style: TextStyle(
-                    fontFamily: 'PoppinsRegular',
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                verticalSpace(10),
-                
-                BlocListener<AuthBloc, AuthState>(
-                  listener: (context, state) {
-                    if (state is AuthLoading) {
-                      _showLoadingDialog(context);
-                      print('AuthLoading state detected');
-                    } else if (state is AuthSuccess) {
-                      _hideLoadingDialog(context);
-                      Future.delayed(const Duration(milliseconds: 300), () {
-                        Navigator.of(context).pushNamedAndRemoveUntil('/home', (route) => false);
-                        print('Navigating to /home');
-                      });
-                    } else if (state is AuthError) {
-                      _hideLoadingDialog(context);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text(state.error)),
-                      );
-                      print('AuthError state detected: ${state.error}');
-                    }
-                  },
-                  child: GradientButton(
-                    text: 'Login',
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        BlocProvider.of<AuthBloc>(context).add(
-                          AuthLogin(
-                            emailController.text,
-                            passwordController.text,
-                          ),
-                        );
-                      }
-                    },
-                  ),
-                ),
-
-                verticalSpace(17),
-                const CustomGoogle(),
-                verticalSpace(5),
-                const CustomSepar(),
-                verticalSpace(10),
-                GradientButton(
-                  text: 'Register',
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/nav');
-                  },
-                ),
-              ],
-            ),
+                    verticalSpace(20),
+                    CustomInput(
+                      hint: 'Email',
+                      obc: false,
+                      icon: Icons.email,
+                      keyboardType: TextInputType.emailAddress,
+                      mycontroller: emailController,
+                      valid: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Email cannot be empty';
+                        }
+                        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                          return 'Enter a valid email';
+                        }
+                        return null;
+                      },
+                    ),
+                    verticalSpace(16),
+                    CustomInput(
+                      hint: 'Password',
+                      obc: true,
+                      icon: Icons.lock,
+                      keyboardType: TextInputType.visiblePassword,
+                      mycontroller: passwordController,
+                      valid: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Password cannot be empty';
+                        }
+                        if (value.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        }
+                        return null;
+                      },
+                    ),
+                    verticalSpace(11),
+                    const Text(
+                      'Forget your password ?',
+                      style: TextStyle(
+                        fontFamily: 'PoppinsRegular',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    verticalSpace(10),
+                    GradientButton(
+                      text: 'Login',
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          BlocProvider.of<AuthBloc>(context).add(
+                            AuthLogin(
+                              emailController.text,
+                              passwordController.text,
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                  
+              verticalSpace(17),
+              const CustomGoogle(),
+              verticalSpace(5),
+              const CustomSepar(),
+              verticalSpace(10),
+              GradientButton(
+                text: 'Register',
+                onPressed: () {
+                  Navigator.pushNamed(context, '/nav');
+                },
+              ),
+            ]),)
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:satim_hack/Logic/auth_bloc/auth_bloc.dart';
+import 'package:satim_hack/Logic/layout/layout_cubit.dart';
 
 import 'package:satim_hack/core/Router/OngenerateRoute.dart';
 import 'package:satim_hack/core/Ui/LogoScreen.dart';
@@ -15,13 +16,21 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthBloc(AuthRepo()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc(AuthRepo()),
+        ),
+        BlocProvider(
+          create: (context) => LayoutCubit(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData.light(useMaterial3: true),
